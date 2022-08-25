c5bf87e8e376d51479852c5cd31b74de8da4a8dc
Agatha07 <agkaranja5@gmail.com>
2022-08-25 01:02:15 +0000

    Print alphabet except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabets in range(ord('a'), ord('z')+1):
-     if (alphabets != 101 and alphabets != 113):
-         print('{:c}'.format(alphabets), end='')
+    if (alphabets != 101 and alphabets != 113):
+        print('{:c}'.format(alphabets), end='')
