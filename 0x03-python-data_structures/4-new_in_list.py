a1efd1d260fbfa90f4309124e80910c3d6a706cf
Agatha07 <agkaranja5@gmail.com>
2022-08-31 00:58:25 +0000

    Replace an element in a list at a specific position

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     if idx >= len(my_list):
         return my_list
-    copy = list(my_list)
+    #copy = list(my_list)
     copy[idx] = element
     return copy
