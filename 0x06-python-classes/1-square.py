d51d858342e62d2622c069aec4d539c71c184363
Agatha07 <agkaranja5@gmail.com>
2022-09-15 03:00:08 +0000

    Fix error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,9 +6,8 @@ class Square:
     """ Represents a square"""
     def __init__(self, size):
         """Initializes a new square
+
         Args:
             size (int): size of the square
-        
-        Returns: None
         """
         self.__size = size
