e7f8efb793a745dc50fad10c401876578a410dcd
Agatha07 <agkaranja5@gmail.com>
2022-09-15 02:48:46 +0000

    Fix error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,15 +3,12 @@
 
 
 class Square:
-    """Represents a square
-    Private instance attribute: size
-    Instantiation with optional size
-    """
+    """Represents a square."""
 
     def __init__(self, size=0):
         """Initializes a square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
