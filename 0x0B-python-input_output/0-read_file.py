4839afa16dbcc97543087da35b82a843f31a5eea
Agatha07 <agkaranja5@gmail.com>
2022-10-24 01:19:20 +0000

    reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module 0-read_file.
+Defines a read text file function."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout:"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
